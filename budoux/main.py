9ae24bf27214be47d4aba55f05300baa99e6147f
eggplants <w10776e8w@yahoo.co.jp>
2021-11-19 13:51:21 +0900

    add: docs

--- budoux/main.py
+++ budoux/main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""BudouX Script to provide CLI for user."""
 import argparse
 import json
 import os
@@ -14,6 +28,17 @@ __version__ = "0.0.1"
 
 
 def check_file(path: str) -> str:
+    """Check if filepath is exist or not.
+
+    Args:
+        path (str): Model path
+
+    Raises:
+        FileNotFoundError: Raise if given path is not exist.
+
+    Returns:
+        str: Model path confirmed its existance.
+    """
     if os.path.isfile(path):
         return path
     else:
@@ -21,7 +46,14 @@ def check_file(path: str) -> str:
 
 
 def parse_args(test: typing.Optional[typing.List[str]] = None) -> argparse.Namespace:
-    """Parse arguments."""
+    """Parse commandline arguments.
+
+    Args:
+        test (typing.Optional[typing.List[str]], optional): Commandline args for testing. Defaults to None.
+
+    Returns:
+        argparse.Namespace: Parsed data of args.
+    """
     parser = argparse.ArgumentParser(
         prog="budoux",
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -32,15 +64,12 @@ def parse_args(test: typing.Optional[typing.List[str]] = None) -> argparse.Names
         ),
     )
 
-    parser.add_argument("text", metavar="TXT", type=str, help="text")
+    parser.add_argument("text", metavar="TXT", nargs="?", type=str, help="text")
     parser.add_argument(
         "-H",
         "--html",
         action="store_true",
         help="HTML mode",
-    )
-    parser.add_argument(
-        "-s", "--stdin", action="store_true", help="Read input from stdin"
     )
     parser.add_argument(
         "-m",
@@ -64,8 +93,8 @@ def main():
 
     parser = budoux.Parser(model)
 
-    if args.stdin:
-        inputs = sys.stdin.read()
+    if args.text is None:
+        inputs = sys.stdin.read().rstrip()
     else:
         inputs = args.text
 
