ba331f91cfd9f01a0c762452e06c96883f7ed3e3
Shuhei Iitsuka <tushuhei@google.com>
2021-11-24 13:18:17 +0900

    Fix Python code style
    Change-Id: Ia9364b64a125e57e4bff35e0c2646b88f18e7527

--- scripts/load_knbc.py
+++ scripts/load_knbc.py
@@ -27,38 +27,40 @@ RESOURCE_URL = (
 
 
 class KNBCHTMLParser(HTMLParser):
-    """Parses the HTML files in the KNBC corpus and outputs the chunks."""
-    def __init__(self, split_tab: bool = True):
-        super().__init__()
-        self.chunks = ['']
-        self.n_rows = 0
-        self.n_cols = 0
-        self.current_word = None
-        self.split_tab = split_tab
+  """Parses the HTML files in the KNBC corpus and outputs the chunks."""
 
-    def handle_starttag(self, tag, _):
-        if tag == 'tr':
-            self.n_rows += 1
-            self.n_cols = 0
-            self.current_word = None
-        if tag == 'td':
-            self.n_cols += 1
+  def __init__(self, split_tab: bool = True):
+    super().__init__()
+    self.chunks = ['']
+    self.n_rows = 0
+    self.n_cols = 0
+    self.current_word = None
+    self.split_tab = split_tab
 
-    def handle_endtag(self, tag):
-        if tag != 'tr': return
-        if (self.n_rows > 2 and self.n_cols == 1
-                and (self.split_tab or self.current_word == '文節区切り')):
-            self.chunks.append('')
-        if self.n_cols == 5:
-            self.chunks[-1] += self.current_word
+  def handle_starttag(self, tag, _):
+    if tag == 'tr':
+      self.n_rows += 1
+      self.n_cols = 0
+      self.current_word = None
+    if tag == 'td':
+      self.n_cols += 1
 
-    def handle_data(self, data):
-        if self.n_cols == 1:
-            self.current_word = data
+  def handle_endtag(self, tag):
+    if tag != 'tr':
+      return
+    if (self.n_rows > 2 and self.n_cols == 1 and
+        (self.split_tab or self.current_word == '文節区切り')):
+      self.chunks.append('')
+    if self.n_cols == 5:
+      self.chunks[-1] += self.current_word
+
+  def handle_data(self, data):
+    if self.n_cols == 1:
+      self.current_word = data
 
 
 def break_before_open_parentheses(chunks: typing.List[str]):
-    """Adds chunk breaks before every open parentheses.
+  """Adds chunk breaks before every open parentheses.
 
   Args:
     chunks (List[str]): Source chunks.
@@ -66,19 +68,20 @@ def break_before_open_parentheses(chunks: typing.List[str]):
   Returns:
     Processed chunks.
   """
-    out: typing.List[str] = []
-    for chunk in chunks:
-        if '（' in chunk:
-            index = chunk.index('（')
-            if index > 0: out.append(chunk[:index])
-            out.append(chunk[index:])
-        else:
-            out.append(chunk)
-    return out
+  out: typing.List[str] = []
+  for chunk in chunks:
+    if '（' in chunk:
+      index = chunk.index('（')
+      if index > 0:
+        out.append(chunk[:index])
+      out.append(chunk[index:])
+    else:
+      out.append(chunk)
+  return out
 
 
 def postprocess(chunks: typing.List[str]):
-    """Applies some processes to modify the extracted chunks.
+  """Applies some processes to modify the extracted chunks.
 
   Args:
     chunks (List[str]): Source chunks.
@@ -86,51 +89,54 @@ def postprocess(chunks: typing.List[str]):
   Returns:
     Processed chunks.
   """
-    chunks = break_before_open_parentheses(chunks)
-    return chunks
+  chunks = break_before_open_parentheses(chunks)
+  return chunks
 
 
 def download_knbc(target_dir: str):
-    """Downloads the KNBC corpus and extracts files.
+  """Downloads the KNBC corpus and extracts files.
 
   Args:
     target_dir: A path to the directory to expand files.
   """
-    os.makedirs(target_dir, exist_ok=True)
-    download_file_path = os.path.join(target_dir, 'knbc.tar.bz2')
-    try:
-        urllib.request.urlretrieve(RESOURCE_URL, download_file_path)
-    except urllib.error.HTTPError:
-        print(f'\033[91mResource unavailable: {RESOURCE_URL}\033[0m')
-        raise
-    with tarfile.open(download_file_path, 'r:bz2') as t:
-        t.extractall(path=target_dir)
+  os.makedirs(target_dir, exist_ok=True)
+  download_file_path = os.path.join(target_dir, 'knbc.tar.bz2')
+  try:
+    urllib.request.urlretrieve(RESOURCE_URL, download_file_path)
+  except urllib.error.HTTPError:
+    print(f'\033[91mResource unavailable: {RESOURCE_URL}\033[0m')
+    raise
+  with tarfile.open(download_file_path, 'r:bz2') as t:
+    t.extractall(path=target_dir)
 
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='''File path to output the training data.
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument(
+      '-o',
+      '--outfile',
+      help='''File path to output the training data.
             (default: source.txt)''',
-                        default='source.txt')
-    args = parser.parse_args()
-    outfile = args.outfile
-    html_dir = 'data/KNBC_v1.0_090925_utf8/html/'
-    if not os.path.isdir(html_dir):
-        download_knbc('data')
-    with open(outfile, 'w') as f:
-        for file in sorted(os.listdir(html_dir)):
-            if file[-11:] != '-morph.html': continue
-            parser = KNBCHTMLParser(split_tab=False)
-            data = open(os.path.join(html_dir, file)).read()
-            parser.feed(data)
-            chunks = parser.chunks
-            chunks = postprocess(chunks)
-            if len(chunks) < 2: continue
-            f.write(utils.SEP.join(chunks) + '\n')
-    print('\033[92mTraining data is output to: %s\033[0m' % (outfile))
+      default='source.txt')
+  args = parser.parse_args()
+  outfile = args.outfile
+  html_dir = 'data/KNBC_v1.0_090925_utf8/html/'
+  if not os.path.isdir(html_dir):
+    download_knbc('data')
+  with open(outfile, 'w') as f:
+    for file in sorted(os.listdir(html_dir)):
+      if file[-11:] != '-morph.html':
+        continue
+      parser = KNBCHTMLParser(split_tab=False)
+      data = open(os.path.join(html_dir, file)).read()
+      parser.feed(data)
+      chunks = parser.chunks
+      chunks = postprocess(chunks)
+      if len(chunks) < 2:
+        continue
+      f.write(utils.SEP.join(chunks) + '\n')
+  print('\033[92mTraining data is output to: %s\033[0m' % (outfile))
 
 
 if __name__ == '__main__':
-    main()
+  main()
--- scripts/train.py
+++ scripts/train.py
@@ -22,7 +22,7 @@ EPS = np.finfo(float).eps
 
 
 def preprocess(entries_filename: str, feature_thres: int):
-    """Loads entries and translates them into NumPy arrays.
+  """Loads entries and translates them into NumPy arrays.
 
   Args:
     entries_filename (str): A file path to the entries file.
@@ -34,36 +34,37 @@ def preprocess(entries_filename: str, feature_thres: int):
     Y (numpy.ndarray): Output labels.
     features (List[str]): Effective features.
   """
-    with open(entries_filename) as f:
-        entries = [row.strip().split('\t') for row in f.read().splitlines()]
-    print('#entries:\t%d' % (len(entries)))
+  with open(entries_filename) as f:
+    entries = [row.strip().split('\t') for row in f.read().splitlines()]
+  print('#entries:\t%d' % (len(entries)))
 
-    features_counter: typing.Counter[str] = Counter()
-    for entry in entries:
-        features_counter.update(entry[1:])
-    features = [
-        item[0] for item in features_counter.most_common()
-        if item[1] > feature_thres
-    ]
-    print('#features:\t%d' % (len(features)))
-    feature_index = dict([(feature, i) for i, feature in enumerate(features)])
+  features_counter: typing.Counter[str] = Counter()
+  for entry in entries:
+    features_counter.update(entry[1:])
+  features = [
+      item[0]
+      for item in features_counter.most_common()
+      if item[1] > feature_thres
+  ]
+  print('#features:\t%d' % (len(features)))
+  feature_index = dict([(feature, i) for i, feature in enumerate(features)])
 
-    M = len(features) + 1
-    N = len(entries)
-    Y = np.zeros(N, dtype=bool)
-    X = np.zeros((N, M), dtype=bool)
+  M = len(features) + 1
+  N = len(entries)
+  Y = np.zeros(N, dtype=bool)
+  X = np.zeros((N, M), dtype=bool)
 
-    for i, entry in enumerate(entries):
-        Y[i] = entry[0] == '1'
-        for col in entry[1:]:
-            if col in feature_index:
-                X[i, feature_index[col]] = True
-        X[i, -1] = True    # add a bias column.
-    return X, Y, features
+  for i, entry in enumerate(entries):
+    Y[i] = entry[0] == '1'
+    for col in entry[1:]:
+      if col in feature_index:
+        X[i, feature_index[col]] = True
+    X[i, -1] = True  # add a bias column.
+  return X, Y, features
 
 
 def pred(phis: typing.Dict[int, float], X: np.ndarray) -> np.ndarray:
-    """Predicts the output from the given classifiers and input entries.
+  """Predicts the output from the given classifiers and input entries.
 
   Args:
     phis (Dict[int, float]): Classifiers represented as a mapping from the
@@ -73,17 +74,16 @@ def pred(phis: typing.Dict[int, float], X: np.ndarray) -> np.ndarray:
   Returns:
     A list of inferred labels.
   """
-    alphas = np.array(list(phis.values()))
-    y = 2 * (X[:, list(phis.keys())] == True) - 1
-    return y.dot(alphas) > 0
+  alphas = np.array(list(phis.values()))
+  y = 2 * (X[:, list(phis.keys())] == True) - 1
+  return y.dot(alphas) > 0
 
 
 def split_dataset(
-        X: np.ndarray,
-        Y: np.ndarray,
-        split_ratio=0.9
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """Splits given entries and labels to training and testing datasets.
+    X: np.ndarray,
+    Y: np.ndarray,
+    split_ratio=0.9) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+  """Splits given entries and labels to training and testing datasets.
 
   Args:
     X (numpy.ndarray): Entries to split.
@@ -96,19 +96,19 @@ def split_dataset(
     Y_train (numpy.ndarray): Training labels.
     Y_test (numpy.ndarray): Testing labels.
   """
-    N, _ = X.shape
-    np.random.seed(0)
-    indices = np.random.permutation(N)
-    X_train = X[indices[:int(N * split_ratio)]]
-    X_test = X[indices[int(N * split_ratio):]]
-    Y_train = Y[indices[:int(N * split_ratio)]]
-    Y_test = Y[indices[int(N * split_ratio):]]
-    return X_train, X_test, Y_train, Y_test
+  N, _ = X.shape
+  np.random.seed(0)
+  indices = np.random.permutation(N)
+  X_train = X[indices[:int(N * split_ratio)]]
+  X_test = X[indices[int(N * split_ratio):]]
+  Y_train = Y[indices[:int(N * split_ratio)]]
+  Y_test = Y[indices[int(N * split_ratio):]]
+  return X_train, X_test, Y_train, Y_test
 
 
 def fit(X: np.ndarray, Y: np.ndarray, features: typing.List[str], iters: int,
         weights_filename: str, log_filename: str):
-    """Trains an AdaBoost classifier.
+  """Trains an AdaBoost classifier.
 
   Args:
     X (numpy.ndarray): Training entries.
@@ -121,79 +121,80 @@ def fit(X: np.ndarray, Y: np.ndarray, features: typing.List[str], iters: int,
   Returns:
     phi (Dict[int, float]): Leanred child classifiers.
   """
-    with open(weights_filename, 'w') as f:
-        f.write('')
-    with open(log_filename, 'w') as f:
-        f.write('')
-    print('Outputting learned weights to %s ...' % (weights_filename))
+  with open(weights_filename, 'w') as f:
+    f.write('')
+  with open(log_filename, 'w') as f:
+    f.write('')
+  print('Outputting learned weights to %s ...' % (weights_filename))
 
-    phis: typing.Dict[int, float] = dict()
-    X_train, X_test, Y_train, Y_test = split_dataset(X, Y)
-    N_train, _ = X_train.shape
-    w = np.ones(N_train) / N_train
+  phis: typing.Dict[int, float] = dict()
+  X_train, X_test, Y_train, Y_test = split_dataset(X, Y)
+  N_train, _ = X_train.shape
+  w = np.ones(N_train) / N_train
 
-    for t in range(iters):
-        print('=== %s ===' % (t))
-        res: np.ndarray = w.dot(Y_train[:, None] ^ X_train) / w.sum()
-        err = 0.5 - np.abs(res - 0.5)
-        m_best = int(err.argmin())
-        pol_best = res[m_best] < 0.5
-        err_min = err[m_best]
-        print('min error:\t', err_min)
-        print('best tree:\t', m_best)
-        alpha = np.log((1 - err_min) / (err_min + EPS))
-        phis.setdefault(m_best, 0)
-        phis[m_best] += alpha if pol_best else -alpha
-        miss = Y_train ^ X_train[:, m_best]
-        if not pol_best: miss = ~(miss)
-        w = w * np.exp(alpha * miss)
-        with open(weights_filename, 'a') as f:
-            feature = features[m_best] if m_best < len(features) else 'BIAS'
-            f.write('%s\t%.3f\n' % (feature, alpha if pol_best else -alpha))
-        acc_train = (pred(phis, X_train) == Y_train).mean()
-        acc_test = (pred(phis, X_test) == Y_test).mean()
-        print('training accuracy:\t', acc_train)
-        print('testing accuracy:\t', acc_test)
-        with open(log_filename, 'a') as f:
-            f.write('%.5f\t%.5f\n' % (acc_train, acc_test))
-    return phis
+  for t in range(iters):
+    print('=== %s ===' % (t))
+    res: np.ndarray = w.dot(Y_train[:, None] ^ X_train) / w.sum()
+    err = 0.5 - np.abs(res - 0.5)
+    m_best = int(err.argmin())
+    pol_best = res[m_best] < 0.5
+    err_min = err[m_best]
+    print('min error:\t', err_min)
+    print('best tree:\t', m_best)
+    alpha = np.log((1 - err_min) / (err_min + EPS))
+    phis.setdefault(m_best, 0)
+    phis[m_best] += alpha if pol_best else -alpha
+    miss = Y_train ^ X_train[:, m_best]
+    if not pol_best:
+      miss = ~(miss)
+    w = w * np.exp(alpha * miss)
+    with open(weights_filename, 'a') as f:
+      feature = features[m_best] if m_best < len(features) else 'BIAS'
+      f.write('%s\t%.3f\n' % (feature, alpha if pol_best else -alpha))
+    acc_train = (pred(phis, X_train) == Y_train).mean()
+    acc_test = (pred(phis, X_test) == Y_test).mean()
+    print('training accuracy:\t', acc_train)
+    print('testing accuracy:\t', acc_test)
+    with open(log_filename, 'a') as f:
+      f.write('%.5f\t%.5f\n' % (acc_train, acc_test))
+  return phis
 
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('encoded_train_data',
-                        help='File path for the encoded training data.')
-    parser.add_argument(
-        '-o',
-        '--output',
-        help='Output file path for the learned weights. (default: weights.txt)',
-        default='weights.txt')
-    parser.add_argument(
-        '--log',
-        help='Output file path for the training log. (default: train.log)',
-        default='train.log')
-    parser.add_argument(
-        '--feature-thres',
-        help='Threshold value of the minimum feature frequency. (default: 10)',
-        default=10)
-    parser.add_argument(
-        '--iter',
-        help='Number of iterations for training. (default: 10000)',
-        default=10000)
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument(
+      'encoded_train_data', help='File path for the encoded training data.')
+  parser.add_argument(
+      '-o',
+      '--output',
+      help='Output file path for the learned weights. (default: weights.txt)',
+      default='weights.txt')
+  parser.add_argument(
+      '--log',
+      help='Output file path for the training log. (default: train.log)',
+      default='train.log')
+  parser.add_argument(
+      '--feature-thres',
+      help='Threshold value of the minimum feature frequency. (default: 10)',
+      default=10)
+  parser.add_argument(
+      '--iter',
+      help='Number of iterations for training. (default: 10000)',
+      default=10000)
 
-    args = parser.parse_args()
-    train_data_filename = args.encoded_train_data
-    weights_filename = args.output
-    log_filename = args.log
-    feature_thres = int(args.feature_thres)
-    iterations = int(args.iter)
+  args = parser.parse_args()
+  train_data_filename = args.encoded_train_data
+  weights_filename = args.output
+  log_filename = args.log
+  feature_thres = int(args.feature_thres)
+  iterations = int(args.iter)
 
-    X, Y, features = preprocess(train_data_filename, feature_thres)
-    fit(X, Y, features, iterations, weights_filename, log_filename)
+  X, Y, features = preprocess(train_data_filename, feature_thres)
+  fit(X, Y, features, iterations, weights_filename, log_filename)
 
-    print('Training done. Export the model by passing %s to build_model.py' %
-          (weights_filename))
+  print('Training done. Export the model by passing %s to build_model.py' %
+        (weights_filename))
 
 
 if __name__ == '__main__':
-    main()
+  main()
