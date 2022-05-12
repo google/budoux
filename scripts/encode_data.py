c1f0491ae348446cfeff21a7db54e127444b6b67
Shuhei Iitsuka <tushuhei@google.com>
2022-05-12 10:30:38 +0900

    Bulk write for faster data encoding

--- scripts/encode_data.py
+++ scripts/encode_data.py
@@ -14,8 +14,41 @@
 """Encodes the training data with extracted features."""
 
 import argparse
+import itertools
+import sys
+from context import feature_extractor, utils
 
-from context import feature_extractor
+
+def process(line: str, entries_filename: str) -> None:
+  """Extratcs features from a source sentence and outputs trainig data entries.
+
+  Args:
+    source_filename (str): A file path to the source sentences.
+    entries_filename (str): A file path to the output entries.
+  """
+  chunks = line.strip().split(utils.SEP)
+  chunk_lengths = [len(chunk) for chunk in chunks]
+  sep_indices = set(itertools.accumulate(chunk_lengths, lambda x, y: x + y))
+  sentence = ''.join(chunks)
+  p1 = utils.Result.UNKNOWN.value
+  p2 = utils.Result.UNKNOWN.value
+  p3 = utils.Result.UNKNOWN.value
+  lines = []
+  for i in range(1, len(sentence) + 1):
+    feature = feature_extractor.get_feature(
+        sentence[i - 3] if i > 2 else utils.INVALID,
+        sentence[i - 2] if i > 1 else utils.INVALID, sentence[i - 1],
+        sentence[i] if i < len(sentence) else utils.INVALID,
+        sentence[i + 1] if i + 1 < len(sentence) else utils.INVALID,
+        sentence[i + 2] if i + 2 < len(sentence) else utils.INVALID, p1, p2, p3)
+    positive = i in sep_indices
+    p = utils.Result.POSITIVE.value if positive else utils.Result.NEGATIVE.value
+    lines.append('\t'.join(['1' if positive else '-1'] + feature) + '\n')
+    p1 = p2
+    p2 = p3
+    p3 = p
+  with open(entries_filename, 'a', encoding=sys.getdefaultencoding()) as f:
+    f.write(''.join(lines))
 
 
 def main() -> None:
@@ -31,10 +64,14 @@ def main() -> None:
       default='encoded_data.txt')
   args = parser.parse_args()
   source_filename = args.source_data
-  train_data_filename = args.outfile
-  feature_extractor.process(source_filename, train_data_filename)
-  print('\033[92mEncoded training data is output to: %s\033[0m' %
-        (train_data_filename))
+  entries_filename = args.outfile
+  with open(source_filename, encoding=sys.getdefaultencoding()) as f:
+    data = f.readlines()
+  with open(entries_filename, 'w', encoding=sys.getdefaultencoding()) as f:
+    f.write('')
+  for line in data:
+    process(line, entries_filename)
+  print('\033[92mEncoded training data is output to: %s\033[0m' % entries_filename)
 
 
 if __name__ == '__main__':
