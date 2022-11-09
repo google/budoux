084b6d053f9ac644fe708c3187250facc15b174a
Shuhei Iitsuka <tushuhei@google.com>
2022-11-09 15:05:54 +0900

    Apply multiprocessing

--- scripts/encode_data.py
+++ scripts/encode_data.py
@@ -16,34 +16,22 @@
 import argparse
 import itertools
 import sys
+import multiprocessing
+import typing
+import functools
 
 from budoux import feature_extractor, utils
 
-
-def process(line: str, entries_filename: str) -> None:
-  """Extratcs features from a source sentence and outputs trainig data entries.
-
-  Args:
-    source_filename (str): A file path to the source sentences.
-    entries_filename (str): A file path to the output entries.
-  """
-  chunks = line.strip().split(utils.SEP)
-  chunk_lengths = [len(chunk) for chunk in chunks]
-  sep_indices = set(itertools.accumulate(chunk_lengths, lambda x, y: x + y))
-  sentence = ''.join(chunks)
-  lines = []
-  for i in range(1, len(sentence) + 1):
-    feature = feature_extractor.get_feature(
-        sentence[i - 3] if i > 2 else utils.INVALID,
-        sentence[i - 2] if i > 1 else utils.INVALID, sentence[i - 1],
-        sentence[i] if i < len(sentence) else utils.INVALID,
-        sentence[i + 1] if i + 1 < len(sentence) else utils.INVALID,
-        sentence[i + 2] if i + 2 < len(sentence) else utils.INVALID)
-    positive = i in sep_indices
-    lines.append('\t'.join(['1' if positive else '-1'] + feature) + '\n')
-  with open(entries_filename, 'a', encoding=sys.getdefaultencoding()) as f:
-    f.write(''.join(lines))
-
+def process(i: int, sentence: str, sep_indices: typing.Set[int]) -> str:
+  feature = feature_extractor.get_feature(
+      sentence[i - 3] if i > 2 else utils.INVALID,
+      sentence[i - 2] if i > 1 else utils.INVALID, sentence[i - 1],
+      sentence[i] if i < len(sentence) else utils.INVALID,
+      sentence[i + 1] if i + 1 < len(sentence) else utils.INVALID,
+      sentence[i + 2] if i + 2 < len(sentence) else utils.INVALID)
+  positive = i in sep_indices
+  line = '\t'.join(['1' if positive else '-1'] + feature)
+  return line
 
 def main() -> None:
   parser = argparse.ArgumentParser(description=__doc__)
@@ -63,9 +51,18 @@ def main() -> None:
     data = f.read().replace('\n', utils.SEP)
   with open(entries_filename, 'w', encoding=sys.getdefaultencoding()) as f:
     f.write('')
-  process(data, entries_filename)
-  print('\033[92mEncoded training data is output to: %s\033[0m' %
-        entries_filename)
+  chunks = data.strip().split(utils.SEP)
+  chunk_lengths = [len(chunk) for chunk in chunks]
+  sep_indices = set(itertools.accumulate(chunk_lengths, lambda x, y: x + y))
+  sentence = ''.join(chunks)
+
+  with multiprocessing.Pool(None) as p:
+    lines = p.map(functools.partial(process, sentence=sentence, sep_indices=sep_indices), range(1, len(sentence) + 1))
+
+  with open(entries_filename, 'a', encoding=sys.getdefaultencoding()) as f:
+    f.write('\n'.join(lines))
+
+  print('\033[92mEncoded training data is out at: %s\033[0m' % entries_filename)
 
 
 if __name__ == '__main__':
