dc3dbe0b924399a511a5964ad7b0ccab71734d54
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 07:25:21 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/load_knbc.py
+++ scripts/load_knbc.py
@@ -109,7 +109,26 @@ def download_knbc(target_dir: str) -> None:
     print(f'\033[91mResource unavailable: {RESOURCE_URL}\033[0m')
     raise
   with tarfile.open(download_file_path, 'r:bz2') as t:
-    t.extractall(path=target_dir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(t, path=target_dir)
 
 
 def parse_args() -> argparse.Namespace:
