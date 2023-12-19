9305a5398fb5bb87c239ee6e371950d5451f58f2
Amit Markel <amarkel@google.com>
2023-12-19 18:59:56 +0200

    Bail-out earlier on the score summation loop to improve performance.

--- javascript/src/parser.ts
+++ javascript/src/parser.ts
@@ -72,12 +72,15 @@ export class Parser {
       score += this.model.get('UW1')?.get(sentence.substring(i - 3, i - 2)) || 0;
       score += this.model.get('UW2')?.get(sentence.substring(i - 2, i - 1)) || 0;
       score += this.model.get('UW3')?.get(sentence.substring(i - 1, i)) || 0;
+      if (score > 0) { result.push(i); continue; }
       score += this.model.get('UW4')?.get(sentence.substring(i, i + 1)) || 0;
       score += this.model.get('UW5')?.get(sentence.substring(i + 1, i + 2)) || 0;
       score += this.model.get('UW6')?.get(sentence.substring(i + 2, i + 3)) || 0;
+      if (score > 0) { result.push(i); continue; }
       score += this.model.get('BW1')?.get(sentence.substring(i - 2, i)) || 0;
       score += this.model.get('BW2')?.get(sentence.substring(i - 1, i + 1)) || 0;
       score += this.model.get('BW3')?.get(sentence.substring(i, i + 2)) || 0;
+      if (score > 0) { result.push(i); continue; }
       score += this.model.get('TW1')?.get(sentence.substring(i - 3, i)) || 0;
       score += this.model.get('TW2')?.get(sentence.substring(i - 2, i + 1)) || 0;
       score += this.model.get('TW3')?.get(sentence.substring(i - 1, i + 2)) || 0;
